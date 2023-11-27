8fd99ecc9657ee011688ff51d7134770075451bf
Alec <alex_cr31@hotmail.com>
2023-11-27 13:10:35 +0100

    Creacion de fichero para upload
    Creación del fichero para poder hacer pruebas del metodo upload

--- test/test_blob.py
+++ test/test_blob.py
@@ -27,6 +27,15 @@ def test_blob_service(proxy_str):
         if not blob_service:
             raise RuntimeError("Invalid proxy")
 
+        # Obtener el directorio de trabajo actual y ruta de donde va a estar el archivo
+        current_directory = os.getcwd()
+        file_path = current_directory+"/fileForUpload.txt"
+        file_path1 = current_directory+"/fileForUpload1.txt"
+
+        #Crea un archivo de prueba si no existe.
+        create_file(file_path,"Contenido de prueba\n")
+        create_file(file_path1,"Contenido de prueba\n")
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Uso: test_blob.py <proxy_del_servicio>")
