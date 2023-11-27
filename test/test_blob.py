fe0e70b4bfb97370bf1008d60b7f24a1c108cc7e
Alec <alex_cr31@hotmail.com>
2023-11-27 13:24:23 +0100

    Testing metodo Upload
    Testing metodo upload donde comprobamos 3 test:
    -Subida
    -Subida mismo contenido diferente nombre
    -Subida con el mismo nombre

--- test/test_blob.py
+++ test/test_blob.py
@@ -27,6 +27,10 @@ def test_blob_service(proxy_str):
         if not blob_service:
             raise RuntimeError("Invalid proxy")
 
+        # Crear un adaptador de objetos para el cliente
+        adapter = communicator.createObjectAdapterWithEndpoints("ClientAdapter", "default -p 10003")
+        adapter.activate()
+
         # Obtener el directorio de trabajo actual y ruta de donde va a estar el archivo
         current_directory = os.getcwd()
         file_path = current_directory+"/fileForUpload.txt"
@@ -35,6 +39,51 @@ def test_blob_service(proxy_str):
         #Crea un archivo de prueba si no existe.
         create_file(file_path,"Contenido de prueba\n")
         create_file(file_path1,"Contenido de prueba\n")
+
+        # Crear una instancia de DataTransfer en el cliente
+        client_data = DataTransfer(file_path)
+        data_transfer = adapter.addWithUUID(client_data).ice_toString()
+        data_transfer_proxy = IceDrive.DataTransferPrx.checkedCast(communicator.stringToProxy(data_transfer))
+
+        # Simular la subida de un blob
+        print("TEST 1: Simular la subida de un blob")
+
+        blob_id = blob_service.upload(data_transfer_proxy)
+        print(f"Blob uploaded with ID: {blob_id}\n")
+
+        # Simular la subida de un blob con diferente nombre pero con mismo contenido
+        print("TEST 2: Simular la subida de un blob con diferente nombre pero con mismo contenido")
+
+        client_data = DataTransfer(file_path1)
+        data_transfer = adapter.addWithUUID(client_data).ice_toString()
+        data_transfer_proxy = IceDrive.DataTransferPrx.checkedCast(communicator.stringToProxy(data_transfer))
+
+        blob_id1 = blob_service.upload(data_transfer_proxy)
+
+        if(blob_id == blob_id1):
+            print("Los blobIds son iguales entonces subida no realizada")
+            print(blob_id + " == " + blob_id1 + "\n")           
+        else:   
+            print("Los blobIds deben ser iguales al subir el mismo archivo\n")
+            print(blob_id + " != " + blob_id1 + "\n")
+
+        # Subir el mismo archivo dos veces
+        print("TEST 3: Simular la subida del blob del test 1")
+
+        # Crear una instancia de DataTransfer en el cliente de nuevo
+        client_data = DataTransfer(file_path)
+        data_transfer = adapter.addWithUUID(client_data).ice_toString()
+        data_transfer_proxy = IceDrive.DataTransferPrx.checkedCast(communicator.stringToProxy(data_transfer))
+        blob_id2 = blob_service.upload(data_transfer_proxy)
+
+        if(blob_id == blob_id2):
+            print("Los blobIds son iguales entonces subida no realizada")
+            print(blob_id + " == " + blob_id2 + "\n")
+            
+        else:   
+            print("Los blobIds deben ser iguales al subir el mismo archivo")
+            print(blob_id + " != " + blob_id2 + "\n")
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
